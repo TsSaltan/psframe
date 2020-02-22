77634b4fe33bb1bba5d90d6b41627fd1471ff6a5
Ts.Saltan <TsSaltan@gmail.com>
2020-02-22 18:19:57 +0300

    Fix aliases

--- src-frame/Frame.ps1
+++ src-frame/Frame.ps1
@@ -3,39 +3,20 @@
  # by tssaltan
  #
  # Required PS version: 3.0
- # Framework version: 1.0-dev
+ # Framework version: 1.0.2-dev
  #>
-New-Variable -Name 'PSFrameVersion' -Value '1.0.1-dev' -Option Constant
+New-Variable -Name 'PSFrameVersion' -Value '1.0.2-dev' -Option Constant
 
-function echo($data){
-    Write-host $data
-}
+Set-Alias -Name new -Value New-Object
 
 function dump($object){
     $object | Select-Object -Property *
-}
-
-function new([string] $className){
-    return New-Object $className
 }
 
 function timestamp(){
     $time = Get-Date -uFormat %s
     $sec,$ms = $time.split(',')
     return $sec;
-}
-
-function getCursorPos(){
-    Add-Type -TypeDefinition @'
-        using System.Windows.Forms;
-        using System.Drawing;
-        public class UICursor {
-            public static Point getPos(){
-               return Cursor.Position;                }
-        }
-'@  -ReferencedAssemblies 'System.Windows.Forms.dll','System.Drawing.dll'    
-
-    return [UICursor]::getPos()
 }
 
 function hash($string, $method){
